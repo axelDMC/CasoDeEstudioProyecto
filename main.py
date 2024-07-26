b13356e835c61772e428c2d2784cd4efabdb7b6f
renzo <renzo10.hinostroza.aylas@gmail.com>
2024-07-26 13:00:12 -0500

    Modificar main.py para usar eliminar_columnas_nulas

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hellow world")
+# main.py
+import pandas as pd
+from tratamientoDx import eliminar_columnas_nulas
+
+df = pd.read_csv('diabetes.csv')
+# df = pd.read_csv('D:\\PUCP\\Proyecto\\tareaLocal\\CasoDeEstudioProyecto\\diabetes.csv')
+df = eliminar_columnas_nulas(df)
